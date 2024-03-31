e44a4b250048e3a29b871772b67760b38e216c93
mjangas <01169027@pw.edu.pl>
2024-03-31 20:53:05 +0200

    rewrite menu into a struct - beggining

--- MJworkspace_due_to_different_Board/Temp/Debug/Core/Src/main.d
+++ MJworkspace_due_to_different_Board/Temp/Debug/Core/Src/main.d
@@ -45,7 +45,7 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Middlewares/Third_Party/FreeRTOS/Source/include/queue.h \
  ../Middlewares/Third_Party/FreeRTOS/Source/include/event_groups.h \
  ../Middlewares/Third_Party/FreeRTOS/Source/include/timers.h \
- ../Core/Inc/liquidcrystal_i2c.h
+ ../Core/Inc/menuHandler.h ../Core/Inc/liquidcrystal_i2c.h
 ../Core/Inc/main.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
 ../Core/Inc/stm32f4xx_hal_conf.h:
@@ -93,4 +93,5 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Middlewares/Third_Party/FreeRTOS/Source/include/queue.h:
 ../Middlewares/Third_Party/FreeRTOS/Source/include/event_groups.h:
 ../Middlewares/Third_Party/FreeRTOS/Source/include/timers.h:
+../Core/Inc/menuHandler.h:
 ../Core/Inc/liquidcrystal_i2c.h:
